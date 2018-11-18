259f0dc4e02d64e1ff69996dfa037485aa51d89b
kowrishankar <kowrishhankar.kaneshamoorthy@bbc.co.uk>
2018-11-18 21:19:34 +0000

    open close nav and body content

--- src/app.js
+++ src/app.js
@@ -1,26 +1,32 @@
 import ReactDOM from 'react-dom';
 import styles from './style.css';
-import mouseAnimation from './visualizations/mouse';
-import Nav from './components/nav';
-import Link from './components/link';
+import Navigation from './components/nav';
 
-const navItems = ['Item', 'Item', 'Item', 'Item', 'Item'];
+function openNav() {
+  const sideNav = document.getElementById("navigator");
+
+  if (sideNav.style.display === "none" || sideNav.style.display === "") {
+    sideNav.style.display = "block";
+  } else {
+    sideNav.style.display = "none";
+  }
+}
 
 const App = () => {
   return (
-    <div className={styles.bg}>
-      <Nav>
-        {
-          navItems.map((item) => {
-            return (
-              <Link title={item}/>
-            )
-          })
-        }
-      </Nav>
+    <div>
+      <div className={styles.nav_btn}>
+        <span id="nav_close_btn" onClick={openNav}> &#9776; </span> Data Visualisation
+        <Navigation />
+      </div>
+
+      <div className={styles.content_body}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
     </div>
-  );
+);
 };
 
-ReactDOM.render(<App/>, document.getElementById('main'));
-mouseAnimation();
+ReactDOM.render(
+  <App/>
+, document.getElementById('main'));
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,28 +1,25 @@
 import styles from './_nav.css';
 import { Component } from 'react';
 
-function openNav() {
-  const sideNav = document.getElementById("sideNav");
-  if (sideNav.style.display === "none") {
-    sideNav.style.display = "block";
-  } else {
-    sideNav.style.display = "none";
-  }
-}
-
-export default class Nav extends Component {
+export default class Navigation extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
     return (
-      <div id="sideNav" className={styles['nav-body']}>
-        <div className={styles['nav-close-btn']} onClick={openNav}>
-          &#9776;
-        </div>
-        {this.props.children}
-      </div>
+      <nav id="navigator" className={styles.nav}>
+        <ul>
+          <li><a href="array.html">Array</a>
+          </li>
+          <li><a href="linked.html">LinkedList</a>
+          </li>
+          <li><a href="arraylist.html">ArrayList</a>
+          </li>
+          <li><a href="node.html">NodeList</a>
+          </li>
+        </ul>
+      </nav>
     );
   }
 };
